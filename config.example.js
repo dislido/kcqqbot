2ea9b632b878b8ef7dc0797723711c579f07617b
dislido <909796264@qq.com>
2019-05-17 05:15:45 +0900

    add example config file

---
+++ config.example.js
@@ -0,0 +1,42 @@
+const Help = require('./dist/modules/help');
+const KcTwitter = require('./dist/modules/kc-twitter');
+const JSVM = require('./dist/modules/jsvm');
+const ChatHistory = require('./dist/modules/chat-history');
+const Notify = require('./dist/modules/notify');
+const Repeat = require('./dist/modules/repeat');
+const TimeCall = require('./dist/modules/timecall');
+const Tuling = require('./dist/modules/tuling');
+const KanBirthday = require('./dist/modules/kan-birthday');
+const Misc = require('./dist/modules/misc');
+const Admin = require('./dist/modules/admin');
+
+const MsgTail = require('./dist/plugins/msg-tail');
+const ModuleCommand = require('./dist/plugins/module-command');
+
+const config = {
+  admin: [1145141919],
+  modules: [
+    new Admin(),
+    new Help(),
+    new KcTwitter(),
+    new JSVM(),
+    new ChatHistory(20),
+    new Notify(),
+    new Repeat(),
+    new TimeCall({
+      use: [
+        { group: 810, use: 'kiyoshimo' },
+      ],
+    }),
+    new KanBirthday({}),
+    new Misc(),
+    new Tuling({ apikey: '1234567890abcdef' }),
+  ],
+  plugins: [
+    // new MsgTail('\n from CQNode'),
+	  // new ModuleCommand(),
+  ],
+  prompt: true,
+};
+
+module.exports = config;
--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 import * as CQNode from '@dislido/cqnode';
 
-const cqnode = CQNode.createRobot({ qqid: '191107040', admin: ['909796264'], listenGroups: ['488733372'], modules: [], plugins: [], prompt: true });
+const cqnode = CQNode.createRobot({ admin: [909796264], modules: [], plugins: [], prompt: true });
 
 console.log(cqnode);
