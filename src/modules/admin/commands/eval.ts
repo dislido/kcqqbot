fa31c92e344549683cf8867b4fb4bc67c0c9d2ce
dislido <disliamn@gmail.com>
2020-06-06 12:22:23 +0800

    fix: cqnode2.3.4, eval输出返回结果

--- src/modules/admin/commands/eval.ts
+++ src/modules/admin/commands/eval.ts
@@ -1,8 +1,32 @@
 import { Command } from "../admin-command";
+import CQNode, { Module, CQAPI } from "@dislido/cqnode";
 
 export default {
-  exec(js: string) {
-    eval(js); // eslint-disable-line no-eval
+  exec(this: Module, js: string, { resp, msgData }) {
+    // todo: console
+    const console = {
+      ...global.console,
+    };
+    // todo: proxyAPI
+    const _API = this.cqnode.api;
+    const API = new Proxy({}, {
+      get: (_, handler) => {
+        if (handler === 'group') {
+          return new Proxy(_API, {
+            get: (_, handler) => {
+              const api = _API[handler as keyof CQAPI];
+              if (api) {
+                return api.bind(this, (<CQNode.CQEvent.GroupMessage>msgData).groupId);
+              }
+              return api;
+            },
+          });
+        }
+        return _API[handler as keyof CQAPI];
+      },
+    });
+    const result = eval(js); // eslint-disable-line no-eval
+    resp.reply(`done.\n执行结果：${result}`);
   },
   auth: 100,
   description: '运行js代码: ~$eval (代码内容)',
