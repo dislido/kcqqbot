0944cee22c81ae6bcc8827694f96e988e30aa714
DisLido <dislido@foxmail.com>
2022-12-08 14:59:09 +0800

    feat: 狗管理 领头衔

--- src/modules/dog-admin/index.ts
+++ src/modules/dog-admin/index.ts
@@ -18,48 +18,48 @@ const Cron: FunctionModule = mod => {
 
   mod.on(CQEventType.messageGroup, async ctx => {
     const msg = ctx.textMessage;
-    console.log(msg)
+
     if (!msg.startsWith('狗管理')) return false;
     const commandStr = ctx.event.message.reduce((str, curr) => {
-      if (curr.type === 'text') return str + ` ${curr.text} `;
-      if (curr.type === 'at') return str + ` ${curr.qq} `;
-      if (curr.type === 'image') return str + ` ${curr.url ?? ''} `;
-      return str
-    }, '')
+      if (curr.type === 'text') return `${str} ${curr.text} `;
+      if (curr.type === 'at') return `${str} ${curr.qq} `;
+      if (curr.type === 'image') return `${str} ${curr.url ?? ''} `;
+      return str;
+    }, '');
     const cmd = parseCommand(commandStr);
-    console.log(commandStr, cmd._)
-    const segments = cmd._
-    segments.shift()
-    segments.shift()
-    console.log(segments)
+
+    const segments = cmd._;
+    segments.shift();
+    segments.shift();
+
     if (!segments.length) {
-      ctx.event.reply(help)
-      return true
+      ctx.event.reply(help);
+      return true;
     }
 
     const methodName = segments.shift()!;
     if (methodName === 'help') {
-      const result = Object.entries(cmdMap).map(([name, cmd]) => `${name} ${cmd.desc}`).join('\n')
-      ctx.event.reply(result)
-      return true
+      const result = Object.entries(cmdMap).map(([name, cmdDef]) => `${name} ${cmdDef.desc}`).join('\n');
+      ctx.event.reply(result);
+      return true;
     }
 
-    let senderLevel = 0
+    let senderLevel = 0;
 
-    if (ctx.event.member.is_admin) senderLevel = 10
-    if (ctx.event.member.is_owner) senderLevel = 100
-    const cmdDef = cmdMap[methodName]
+    if (ctx.event.member.is_admin) senderLevel = 10;
+    if (ctx.event.member.is_owner) senderLevel = 100;
+    const cmdDef = cmdMap[methodName];
 
     if (!cmdDef) {
-      ctx.event.reply('没有此命令')
-      return true
+      ctx.event.reply('没有此命令');
+      return true;
     }
 
     if (senderLevel < cmdDef.level) {
-      ctx.event.reply(`权限不足，你的权限是${senderLevel}, 此命令需要权限${cmdDef.level} (管理员权限10，群主权限100，第一次使用可能未加载权限，重试即可)`)
-      return true
+      ctx.event.reply(`权限不足，你的权限是${senderLevel}, 此命令需要权限${cmdDef.level} (管理员权限10，群主权限100，第一次使用可能未加载权限，重试即可)`);
+      return true;
     }
-    await cmdDef.fn(ctx, ...cmd._)
+    await cmdDef.fn(ctx, ...cmd._);
 
     return true;
   });
